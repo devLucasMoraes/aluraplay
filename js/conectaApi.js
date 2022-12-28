75744012e6122f3aa6726a055f04a13f732d8dc8
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-27 21:57:51 -0300

    criado duncao para fazer uma requisicao get usando o fetch e retornando dados como json

---
+++ js/conectaApi.js
@@ -0,0 +1,11 @@
+async function listaVideos() {
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoJson = await conexao.json();
+    console.log(conexaoJson);
+    return conexaoJson;
+}
+
+
+export const conectaApi = {
+    listaVideos
+}
